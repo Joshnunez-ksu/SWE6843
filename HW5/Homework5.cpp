4fc9e491862835eb7373d5fed2ed8219bea6190b
Josh Nunez <jnunez20@students.kennesaw.edu>
2017-02-16 15:48:04 -0500

    updated homework5 to use the driver class

--- HW5/Homework5.cpp
+++ HW5/Homework5.cpp
@@ -3,11 +3,26 @@ Homework 5
 
 Josh Nunez
 Josh Osburn
-Danica Roberts
 
 ********************************************************************************/
 
+#include "HW5_States.h";
+
 int main()
 {
+      PeripheralFactory pf;
+      StateManager stateManager;
+      
+      stateManager.registerState("StateInitial", new StateInitial(&stateManager, &pf));
+      stateManager.registerState("StateBeforeGame", new StateBeforeGame(&stateManager, &pf));
+      stateManager.registerState("StatePreInGame", new StatePreInGame(&stateManager, &pf));
+      stateManager.registerState("StateWaitForOne", new StateWaitForOne(&stateManager, &pf));
+      stateManager.registerState("StatePostGame", new StatePostGame(&stateManager, &pf));
+            
+      GameData theData;
+
+      Driver driver(&stateManager);
+      driver.run(&theData);
+      
       return 0;
 }
