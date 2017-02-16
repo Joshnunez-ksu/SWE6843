3aed2c0a0659d30b85c1047519eab8c034ac94b6
josburn14 <josburn@sea-turtle.net>
2017-02-16 00:16:51 -0500

    Create HW5_States.h

---
+++ HW5/HW5_States.h
@@ -0,0 +1,35 @@
+#include "../common/State.h"
+
+struct GameData
+{
+      long startTick;
+      long playerOneTick;
+      long playerTwoTick;
+};
+
+class StateInitial : public State
+{
+public:
+      StateInitial(StateManager* sm, PeripheralFactory* pf) : State(sm, pf){};
+      State* processInput(void* data);
+};
+
+class StateBeforeGame : public State
+{
+public:
+      StateBeforeGame(StateManager* sm, PeripheralFactory* pf);
+      ~StateBeforeGame();
+      State* processInput(void* data);
+      
+ private:
+      Button* theButton;
+      GPIOPin* LED;
+      int pressCount;
+};
+
+class StateAfterGame : public State
+{
+public:
+      StateAfterGame(StateManager* sm, PeripheralFactory* pf) : State(sm, pf){};
+      State* processInput(void* data);
+};
