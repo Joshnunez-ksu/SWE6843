c700011ac20056fe7bd0f94edd554ebf0b54673f
josburn14 <josburn@sea-turtle.net>
2017-03-01 02:30:03 -0500

    RecipeStates.cpp

--- Project1/RecipeStates.cpp
+++ Project1/RecipeStates.cpp
@@ -16,50 +16,133 @@ long getTick()
 
 State* Initial::process(void* data)
 {
+	State* returnState = this;
+	GPIOSystem* gpio = (GPIOSystem*) this->peripheralFactory->getPeripheral(PERIPHERAL_GPIO);
 	RecipeData* data = (RecipeData*) data;
+
+	//Initialize peripherals
+	//data->display = new Display(gpio->getPin(2));
+	//data->keypad = new Keypad(gpio->getPin(2));
+	//data->scale = new Scale(gpio->getPin(2));
+	//data->currentRecipe = ??;
+
+	return returnState;
 }
 
 State* Welcome::process(void* data)
 {
+	State* returnState = this;
 	RecipeData* data = (RecipeData*) data;
+
+	/*if(data->keypad->getKey("start")->pressed())
+	{
+		returnState = this->stateManager->getState("RecipeList");
+	}*/
+
+	return returnState;
 }
 
 State* RecipeList::process(void* data)
 {
+	State* returnState = this;
 	RecipeData* data = (RecipeData*) data;
+
+	//Show recipes
+	//data->display->setValue(recipes);
+
+	//Get keypad input
+	char userInput = data->keypad->getKey();
+
+	switch(userInput)
+	{
+		case '0':
+			break;
+		case '1':
+			//returnState = this->stateManger->getState("DisplayIngredients" + recipe[1].getState());
+			break;
+		case '2':
+			break;
+		case '3':
+			break;
+		case '4':
+			break;
+		case '5':
+			break;
+		case '6':
+			break;
+		case '7':
+			break;
+		case '8':
+			break;
+		case '9':
+			break;
+		case 'a':
+			//Scroll up if not at the top
+			break;
+		case 'b':
+			break;
+		case 'c':
+			break;
+		case 'd':
+			//Scroll down if not at the bottom
+			break;
+	}
+
+	return returnState;
 }
 
 State* DisplayIngredients::process(void* data)
 {
+	State* returnState = this;
 	RecipeData* data = (RecipeData*) data;
+
+	return returnState;
 }
 
 State* ZeroScale::process(void* data)
 {
+	State* returnState = this;
 	RecipeData* data = (RecipeData*) data;
+
+	return returnState;
 }
 
 State* Fill::process(void* data)
 {
+	State* returnState = this;
 	RecipeData* data = (RecipeData*) data;
+
+	return returnState;
 }
 
 State* Validate::process(void* data)
 {
+	State* returnState = this;
 	RecipeData* data = (RecipeData*) data;
+
+	return returnState;
 }
 
 State* ErrorFill::process(void* data)
 {
+	State* returnState = this;
 	RecipeData* data = (RecipeData*) data;
+
+	return returnState;
 }
 
 State* AdditionalStep::process(void* data)
 {
+	State* returnState = this;
 	RecipeData* data = (RecipeData*) data;
+
+	return returnState;
 }
 
 State* Done::process(void* data)
 {
+	State* returnState = this;
 	RecipeData* data = (RecipeData*) data;
+
+	return returnState;
 }
