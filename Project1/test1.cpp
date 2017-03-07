57edf5c1c2b9a8b404d70581588930703d52be8f
Josh Nunez <jnunez20@students.kennesaw.edu>
2017-03-07 00:52:25 -0500

    test1.cpp

--- Project1/test1.cpp
+++ Project1/test1.cpp
@@ -6,7 +6,8 @@ Test Key pad with Display
 
 int main()
 {
-	GPIOSystem* gpio = (GPIOSystem*) this->peripheralFactory->getPeripheral(PERIPHERAL_GPIO);
+	PeripheralFactory pf;
+	GPIOSystem* gpio = (GPIOSystem*) pf->getPeripheral(PERIPHERAL_GPIO);
 
 	//Initialize data
 	Display* display = new Display(gpio->getPin(20),
@@ -20,18 +21,19 @@ int main()
 					  gpio->getPin(12),
 					  gpio->getPin(4));
 	
-	KeyPad* keypad = new KeyPad(gpio->getPin(26),
+	KeyPad* keypad = new KeyPad(
+					gpio->getPin(5),
+					gpio->getPin(6),
+					gpio->getPin(26),
 					gpio->getPin(19),
 					gpio->getPin(13),
-					gpio->getPin(6),
-					gpio->getPin(5),
 					gpio->getPin(9),
 					gpio->getPin(10),
 					gpio->getPin(22));
 					
 	int currentKey = 0;
 	
-	while((currentKey = keypad->getKey()) != 15)
+	while((currentKey = keypad->getKey()) < 15)
 	{
 		if (currentKey > 9)
 		{
